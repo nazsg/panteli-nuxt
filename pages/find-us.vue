3f0dcb1bd9700f6ea0fda009e56dffc244d0e86a
Naz <post@nazs.net>
2020-05-01 23:19:49 +0100

    added LAN access, image swap on find-us

--- pages/find-us.vue
+++ pages/find-us.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
-    Find Us
+    <div class="image">
+      <img :src="images[img]" alt="">
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      images: [
+        'https://panteli.biz/static/chips.jpg',
+        'https://panteli.biz/static/chips2.jpg',
+        'https://panteli.biz/static/cup-smoke.jpg',
+        'https://panteli.biz/static/coffee2a.jpg',
+        'https://panteli.biz/static/coffee.jpg'
+      ],
+      img: 0
+    }
+  },
+  methods: {
+  },
+  mounted() {
+    setInterval(() => {
+      this.img = Math.floor(Math.random() * 4);     // returns a random integer from 0 to 9
+    }, 5000);    
+  },
   head: {
     title: 'Find Us',
     meta: [
@@ -16,3 +37,14 @@ export default {
   }  
 }
 </script>
+
+<style lang="scss" scoped>
+.image {
+  margin: 0 auto;
+  height: 90vh;
+  img {
+    width: 100%;
+    overflow: hidden;
+  }
+}
+</style>
